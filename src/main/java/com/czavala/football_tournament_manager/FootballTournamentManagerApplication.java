516d2a566b6ae2602109d6c44af89e1c10ecbe96
criszav <czavala.dev@gmail.com>
2024-12-11 16:37:52 -0300

    initial commit

---
+++ src/main/java/com/czavala/football_tournament_manager/FootballTournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.czavala.football_tournament_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTournamentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czavala/football_tournament_manager/FootballTournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czavala.football_tournament_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
