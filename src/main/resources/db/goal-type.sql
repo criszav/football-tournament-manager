4d1fc29832081ce00efcf17f2ce66cb5711d460d
criszav <czavala.dev@gmail.com>
2024-12-19 13:18:49 -0300

    agrega y configura archivos .sql para poblar tablas

---
+++ src/main/resources/db/goal-type.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_goal_type (type_name, description, is_active) VALUES ("Normal", "Gol de jugada", true);
+INSERT INTO t_goal_type (type_name, description, is_active) VALUES ("Own goal", "Autogol", true);
+INSERT INTO t_goal_type (type_name, description, is_active) VALUES ("Penalty", "Gol de penal", true);
+INSERT INTO t_goal_type (type_name, description, is_active) VALUES ("Free kick", "Gol de tiro libre", true);
---
+++ src/main/resources/db/user-role.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_user_role (role_name, description) VALUES ("Super Admin", "Administrador con todos los permisos del sistema (developers)");
+INSERT INTO t_user_role (role_name, description) VALUES ("Admin", "Administrador general del sistema (encargado)");
+INSERT INTO t_user_role (role_name, description) VALUES ("Tournament Manager", "Encargado de gestionar torneos");
+INSERT INTO t_user_role (role_name, description) VALUES ("Team Manager", "Manager y representante de un equipo");
