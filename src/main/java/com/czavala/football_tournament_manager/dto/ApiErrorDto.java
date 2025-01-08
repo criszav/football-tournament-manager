c3944b5c0928bacdacd686a6bb1bb3de423c0c67
criszav <czavala.dev@gmail.com>
2025-01-08 09:03:52 -0300

    agrega implementación de interface Serializable a dto ApiError

--- src/main/java/com/czavala/football_tournament_manager/dto/ApiErrorDto.java
+++ src/main/java/com/czavala/football_tournament_manager/dto/ApiErrorDto.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-public class ApiErrorDto {
+public class ApiErrorDto implements Serializable {
 
     @JsonProperty(value = "http_code")
     private Integer httpCode;
