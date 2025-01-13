8c23c03638eb5604260a46682effc26f7d46d3a3
criszav <czavala.dev@gmail.com>
2025-01-13 08:50:04 -0300

    Card: agrega metodo para encontrar team por id en capa repositorio

--- src/main/java/com/czavala/football_tournament_manager/persistance/repository/CardRepository.java
+++ src/main/java/com/czavala/football_tournament_manager/persistance/repository/CardRepository.java
@@ -1,5 +1,6 @@
 package com.czavala.football_tournament_manager.persistance.repository;
 
+import com.czavala.football_tournament_manager.dto.card.CardTournamentResponseDto;
 import com.czavala.football_tournament_manager.persistance.entity.Card;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,4 +11,6 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     Page<Card> findByTournamentId(Long tournamentId, Pageable pageable);
 
     Page<Card> findByTournamentIdAndTeamId(Long tournamentId, Long teamId, Pageable pageable);
+
+    Page<Card> findByTeamId(Long teamId, Pageable pageable);
 }
