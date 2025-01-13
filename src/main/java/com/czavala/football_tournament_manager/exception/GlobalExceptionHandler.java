ac850c6e3c5805a59269e867b5f1084064b9f6f7
criszav <czavala.dev@gmail.com>
2025-01-13 09:48:57 -0300

    agrega manejo de excepciones generica y de errores de campo
    Manejo de excepciones:
    - MethodArgumentNotValidException
    - Exception

--- src/main/java/com/czavala/football_tournament_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/czavala/football_tournament_manager/exception/GlobalExceptionHandler.java
@@ -4,11 +4,15 @@ import com.czavala.football_tournament_manager.dto.ApiErrorDto;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,15 +24,60 @@ public class GlobalExceptionHandler {
 
         int httpCode = HttpStatus.NOT_FOUND.value();
 
-        ApiErrorDto apiErrorDto = new ApiErrorDto();
-        apiErrorDto.setHttpCode(httpCode);
-        apiErrorDto.setHttpMethod(request.getMethod());
-        apiErrorDto.setUrl(request.getRequestURL().toString());
-        apiErrorDto.setMessage("Recurso solicitado no encontrado. Por favor intente otra busqueda diferente.");
-        apiErrorDto.setBackendMessage(exception.getLocalizedMessage());
-        apiErrorDto.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
-        apiErrorDto.setDetails(null);
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage("Recurso solicitado no encontrado. Por favor intente otra busqueda diferente.");
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(null);
 
-        return ResponseEntity.status(httpCode).body(apiErrorDto);
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                             HttpServletRequest request) {
+
+        List<ObjectError> errors = exception.getAllErrors();
+        List<String> details = errors.stream().map(error -> {
+
+            if (error instanceof FieldError fieldError) {
+                return fieldError.getField() + ": " + fieldError.getDefaultMessage();
+            }
+            return error.getDefaultMessage();
+
+        }).toList();
+
+        int httpCode = HttpStatus.BAD_REQUEST.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage("La petición contiene parámetros inválidos o incompletos. Por favor verifique que la información enviada es correcta.");
+        apiError.setBackendMessage(exception.getMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(details);
+
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorDto> handleGenericException(Exception exception, HttpServletRequest request) {
+
+        int httpCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage("Oops! Algo salió mal. Por favor contacta al administrador o intenta de nuevo más tarde.");
+        apiError.setBackendMessage(exception.getMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(null);
+
+        return ResponseEntity.status(httpCode).body(apiError);
     }
 }
