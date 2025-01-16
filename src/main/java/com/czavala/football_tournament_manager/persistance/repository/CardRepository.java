f07f801439fa302f7bd7875fcfee5e76e7316aea
criszav <czavala.dev@gmail.com>
2025-01-16 09:05:12 -0300

    agrega metodos para obtener cardtype y cards de player por tournament, en capa de repositorio
    - CardRepository

--- src/main/java/com/czavala/football_tournament_manager/persistance/repository/CardRepository.java
+++ src/main/java/com/czavala/football_tournament_manager/persistance/repository/CardRepository.java
@@ -1,5 +1,6 @@
 package com.czavala.football_tournament_manager.persistance.repository;
 
+import com.czavala.football_tournament_manager.dto.card.CardResponseDto;
 import com.czavala.football_tournament_manager.dto.card.CardTournamentResponseDto;
 import com.czavala.football_tournament_manager.persistance.entity.Card;
 import org.springframework.data.domain.Page;
@@ -13,4 +14,8 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     Page<Card> findByTournamentIdAndTeamId(Long tournamentId, Long teamId, Pageable pageable);
 
     Page<Card> findByTeamId(Long teamId, Pageable pageable);
+
+    Page<Card> findByCardType(String cardType, Pageable pageable);
+
+    Page<Card> findByTournamentIdAndPlayerId(Long tournamentId, Long playerId, Pageable pageable);
 }
