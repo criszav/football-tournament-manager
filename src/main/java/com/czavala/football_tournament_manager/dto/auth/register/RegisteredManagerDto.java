ac02d18939de580ae8aeabece00262d29e1996b9
criszav <czavala.dev@gmail.com>
2025-01-27 09:42:42 -0300

    creacion de dto de request y response para registrar un manager

---
+++ src/main/java/com/czavala/football_tournament_manager/dto/auth/register/RegisteredManagerDto.java
@@ -0,0 +1,30 @@
+package com.czavala.football_tournament_manager.dto.auth.register;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class RegisteredManagerDto implements Serializable {
+
+    @JsonProperty("manager_id")
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String username;
+
+    private String email;
+
+    private String role;
+
+    private String jwt;
+
+}
