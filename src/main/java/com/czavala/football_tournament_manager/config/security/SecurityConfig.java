733e9d3d20a23128bef13d1195f69e3427086c07
criszav <czavala.dev@gmail.com>
2025-01-30 09:38:56 -0300

    actualización cadena de filtros SecurityFilterChain en SecurityConfig
    - agrega session management 'stateless'
    - setea el authentication provider
    - agrega filtro personalizado JwtAuthenticationFilter
    - manejo de endpoints publicos y privados

--- src/main/java/com/czavala/football_tournament_manager/config/security/SecurityConfig.java
+++ src/main/java/com/czavala/football_tournament_manager/config/security/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.czavala.football_tournament_manager.config.security;
 
-import com.czavala.football_tournament_manager.config.security.filter.JwtAuthFilter;
+import com.czavala.football_tournament_manager.config.security.filter.JwtAuthenticationFilter;
 import com.czavala.football_tournament_manager.service.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,32 +22,44 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                // Desahbilita csrf, ya que el sistema es STATELESS
                 .csrf(csrf -> csrf.disable())
+
+                // Indica que la app es STATELESS (basada en tokens jwt), Spring Security NO crea un HttpSession
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authenticationProvider(authenticationProvider())
+
+                // Aplica nuestro filtro personalizado
+                // antes de que se aplique el filtro UsernamePasswordAuthenticationFilter en la cadena de filtros
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .authorizeHttpRequests(authRequest -> {
-                    authRequest.requestMatchers("/home/public").permitAll();
-                    authRequest.requestMatchers("/home/logout").permitAll();
+                    authRequest.requestMatchers(HttpMethod.GET, "/home/public").permitAll();
+                    authRequest.requestMatchers(HttpMethod.GET, "/home/logout").permitAll();
+
+                    // Endpoint para login es publico
+                    authRequest.requestMatchers(HttpMethod.POST, "/authenticate/login").permitAll();
+                    authRequest.requestMatchers(HttpMethod.GET, "/authenticate/**").permitAll();
+
+                    // Sólo admin y superadmin pueden registrar a los managers
+                    authRequest.requestMatchers(HttpMethod.POST, "/manager/register")
+                            .hasAnyRole("ADMIN", "SUPER_ADMIN");
+
+
                     authRequest.anyRequest().authenticated();
-                })
-
-                // Agrega nuestro filtro personalizado antes de ejecutar el filtro UsernamePasswordAuthenticationFilter
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-
-                .formLogin(Customizer.withDefaults())
-                .oauth2Login(oauth2 ->
-                        oauth2.defaultSuccessUrl("/home/private"))
-                .logout(logout -> {
-                    logout.logoutSuccessUrl("/home/logout");
-                    logout.invalidateHttpSession(true);
-                    logout.deleteCookies("JSESSIONID");
                 });
 
         return http.build();
@@ -67,6 +80,7 @@ public class SecurityConfig {
         return daoAuthProvider;
     }
 
+    // Indica que BCryptPasswordEncoder es la implementación de PasswordEncoder que se utilizará
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
