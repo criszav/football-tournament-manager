be6c0758d1304ae269b4bd5f1baf4792973d44ab
criszav <czavala.dev@gmail.com>
2025-01-30 09:23:01 -0300

    update insert statements in files .sql

--- src/main/resources/db/tournament-status.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO t_tournament_status (status_name, description, is_active) VALUES ("Upcoming", "Torneo aún no ha comenzado", true);
-INSERT INTO t_tournament_status (status_name, description, is_active) VALUES ("Ongoing", "Torneo se esta jugando", true);
-INSERT INTO t_tournament_status (status_name, description, is_active) VALUES ("Finished", "Torneo finalizado", true);
-INSERT INTO t_tournament_status (status_name, description, is_active) VALUES ("Cancelled", "Torneo ha sido cancelado", true);
--- src/main/resources/db/users.sql
+++ src/main/resources/db/users.sql
@@ -6,10 +6,10 @@ INSERT INTO t_user_role (role_name, description) VALUES ("TEAM_MANAGER", "Manage
 INSERT INTO t_user_role (role_name, description) VALUES ("PLAYER", "Jugador de futbol");
 
 -- Users
-INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Cristian", "Zavala", "czavala", "czavala@gmail.com", "clave123", "123456789", true, 1)
-INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Alexis", "Sanchez", "asanchez", "asanchez@gmail.com", "clave456", "111111111", true, 2)
-INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Cristiano", "Ronaldo", "cronaldo", "cronaldo@gmail.com", "clave789", "222222222", true, 3)
-INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Arturo", "Vidal", "avidal", "avidal@gmail.com", "clave000", "333333333", true, 4)
+INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Cristian", "Zavala", "czavala", "czavala@gmail.com", "$2a$10$qMZy.jrJMwZdhGHrRr23auPoZH0vCEHK4ymA4Ed7VzKig5Lm63a2a", "123456789", true, 1);
+INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Alexis", "Sanchez", "asanchez", "asanchez@gmail.com", "$2a$10$jyOk2X5gqeyTuxxSPYWGXe8Y1fwfwloYMfiV0wwsMyMOEaPjXYdnS", "111111111", true, 2);
+INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Cristiano", "Ronaldo", "cronaldo", "cronaldo@gmail.com", "$2a$10$bDvFaHuYGrzG7rJFT8z6Nu64r.gn4WNLOGNIqpctRTQbHqurQQDuS", "222222222", true, 3);
+INSERT INTO t_user (firstname, lastname, username, email, password, run, is_active, user_role_id) VALUES ("Arturo", "Vidal", "avidal", "avidal@gmail.com", "$2a$10$6i3SBcD7zuk1n4ch.MPtWOegtbaztfC1hahDt5zWREsw1T2LAABX.", "333333333", true, 4);
 
 
 
