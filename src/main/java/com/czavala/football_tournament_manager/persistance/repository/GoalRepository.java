4f7a8e723629f3e62789a882bc0a8dc5be5774ed
criszav <czavala.dev@gmail.com>
2025-02-21 10:17:12 -0300

    feat(stats): agregar metodos en GoalRepository para stats de goles por jugador y equipo
    - Implementar 'findGoalsScoredGroupByPlayer' para obtener listado de goleadores
    - Implementar 'findGoalsScoredGroupByTeam' para obtener listado de goles por equipo
    - Utilizar @Query para optimizar consultas

--- src/main/java/com/czavala/football_tournament_manager/persistance/repository/GoalRepository.java
+++ src/main/java/com/czavala/football_tournament_manager/persistance/repository/GoalRepository.java
@@ -1,10 +1,15 @@
 package com.czavala.football_tournament_manager.persistance.repository;
 
-import com.czavala.football_tournament_manager.dto.goal.GetGoalResponseDto;
 import com.czavala.football_tournament_manager.persistance.entity.Goal;
+import com.czavala.football_tournament_manager.persistance.utils.PlayerGoals;
+import com.czavala.football_tournament_manager.persistance.utils.TeamGoals;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GoalRepository extends JpaRepository<Goal,Long> {
 
@@ -17,4 +22,18 @@ public interface GoalRepository extends JpaRepository<Goal,Long> {
     Page<Goal> findByPlayerId(Long playerId, Pageable pageable);
 
     Page<Goal> findByPlayerIdAndTeamId(Long playerId, Long teamId, Pageable pageable);
+
+    // Obtiene listado de goleadores de un determinado torneo
+    @Query("SELECT g.player.id as playerId, COUNT(g.id) as goals " +
+            "FROM Goal g " +
+            "WHERE g.tournament.id = :tournamentId " +
+            "GROUP BY g.player.id")
+    List<PlayerGoals> findGoalsScoredGroupByPlayer(@Param("tournamentId") Long tournamentId);
+
+    // Obtiene listado de equipos por goles anotados en un determinado torneo
+    @Query("SELECT g.teamId as teamId, COUNT(g.teamId) as goals " +
+            "FROM Goal g " +
+            "WHERE g.tournamentId = :tournamentId " +
+            "GROUP BY g.teamId")
+    List<TeamGoals> findGoalsScoredGroupByTeam(@Param("tournamentId") Long tournamentId);
 }
