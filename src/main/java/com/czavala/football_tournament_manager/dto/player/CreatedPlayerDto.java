3c808884a0343a0c8d34e978833193a37697d0d7
criszav <czavala.dev@gmail.com>
2025-02-27 16:50:02 -0300

    feat(player): agregar dto CreatedPlayerDto para estandizar respuestas al crear un player

---
+++ src/main/java/com/czavala/football_tournament_manager/dto/player/CreatedPlayerDto.java
@@ -0,0 +1,23 @@
+package com.czavala.football_tournament_manager.dto.player;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CreatedPlayerDto {
+
+    @JsonProperty("player_id") private Long id;
+    private String firstname;
+    private String lastname;
+    private String nickname;
+    @JsonProperty("squad_number") private Integer squadNumber;
+    @JsonProperty("image_url") private String imageUrl;
+    @JsonProperty("is_active") private Boolean isActive;
+    @JsonProperty("is_enabled") private Boolean isEnabled;
+    @JsonProperty("team_id") private Long teamId;
+
+}
