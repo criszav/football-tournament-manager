eed48632f65242b68d3ec46198300c7e7dbb6a72
criszav <czavala.dev@gmail.com>
2025-02-28 10:36:43 -0300

    feat(exception): agregar manejo de excepciones para squad number duplicado, teams inválido en match y para manejo de imágenes

--- src/main/java/com/czavala/football_tournament_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/czavala/football_tournament_manager/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.czavala.football_tournament_manager.exception;
 
 import com.czavala.football_tournament_manager.dto.ApiErrorDto;
+import com.czavala.football_tournament_manager.utils.ImageUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,14 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -82,6 +87,81 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(httpCode).body(apiError);
     }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<ApiErrorDto> handleMultipartException(MultipartException exception, HttpServletRequest request) {
+
+        int httpCode = HttpStatus.BAD_REQUEST.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage("Extensión de la imagen es inválida. Por favor cargar una imagen con una extensión válida: .jpg, .jpeg, .png");
+        apiError.setBackendMessage(exception.getMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(Map.of(
+                "valid_extensions", ImageUtils.VALID_IMAGE_EXTENSIONS
+        ));
+
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiErrorDto> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exception, HttpServletRequest request) {
+
+        int httpCode = HttpStatus.BAD_REQUEST.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage("El tamaño de la imagen excede el máximo permitido.");
+        apiError.setBackendMessage(exception.getMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(Map.of(
+                "max_size", "5MB"
+        ));
+
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
+    @ExceptionHandler(DuplicateSquadNumberException.class)
+    public ResponseEntity<ApiErrorDto> handleDuplicateSquadNumberException(
+            DuplicateSquadNumberException exception, HttpServletRequest request) {
+
+        int httpCode = HttpStatus.CONFLICT.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage(exception.getMessage());
+        apiError.setBackendMessage("Unique constraint violation 'UK_PLAYER_SQUAD_TEAM'");
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(null);
+
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
+    @ExceptionHandler(InvalidMatchException.class)
+    public ResponseEntity<ApiErrorDto> handleInvalidMatchException(
+            InvalidMatchException exception, HttpServletRequest request) {
+
+        int httpCode = HttpStatus.BAD_REQUEST.value();
+
+        ApiErrorDto apiError = new ApiErrorDto();
+        apiError.setHttpCode(httpCode);
+        apiError.setHttpMethod(request.getMethod());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMessage(exception.getMessage());
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+        apiError.setDetails(null);
+
+        return ResponseEntity.status(httpCode).body(apiError);
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorDto> handleGenericException(Exception exception, HttpServletRequest request) {
