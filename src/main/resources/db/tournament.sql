02974abb8d8621a2d3e4fcf84818c55897d77b68
criszav <czavala.dev@gmail.com>
2025-03-11 09:27:04 -0300

    feat(data): agregar data inicial de team y tournament para insertar en db
    - team.sql: agregar todos los teams que participan en el futbol profesional de Chile.
    - tournament.sql: agregar los torneos profesionales que se juegan en Chile durante 2025.

--- src/main/resources/db/tournament.sql
+++ src/main/resources/db/tournament.sql
@@ -14,17 +14,54 @@ INSERT INTO t_tournament (name, start_date, end_date, number_of_teams, is_active
 INSERT INTO t_tournament (name, start_date, end_date, number_of_teams, is_active, user_id, tournament_status_id) VALUES ("Copa Chile 2025", '2025-01-26', null, 32, true, 1, 2);
 
 -- Tournament Teams
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 1);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 2);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 3);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 4);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 5);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 6);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 7);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 8);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 9);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 10);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 11);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 12);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 13);
-INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (1, 14);
+
+-- Equipos de Primera 2025
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 1);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 2);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 3);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 4);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 5);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 6);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 7);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 8);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 9);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 10);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 11);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 12);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 13);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 14);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 15);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (3, 16);
+
+-- Equipos de Primera B 2025
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 17);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 18);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 19);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 20);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 21);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 22);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 23);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 24);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 25);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 26);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 27);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 28);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 29);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 30);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 31);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 32);
+
+-- Equipos Segunda 2025
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 33);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 34);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 35);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 36);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 37);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 38);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 39);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 40);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 41);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 42);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 43);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 44);
+INSERT INTO t_tournament_team (tournament_id, team_id) VALUES (4, 45);
