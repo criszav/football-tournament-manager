4b532fcb4d352c79e443398eee2af81724406151
criszav <czavala.dev@gmail.com>
2025-03-17 12:55:55 -0300

    feat(data): agregar data de inserción inicial de match

--- src/main/resources/db/match.sql
+++ src/main/resources/db/match.sql
@@ -61,39 +61,39 @@ VALUES ("2025-03-02", "20:30", 3, 2, 2, 2, 3, 14, 11, null);
 
 -- Fecha 4
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-07", "20:00", 4, null, null, 1, 3, 14, 9, null);
+VALUES ("2025-03-07", "20:00", 4, 1, 0, 1, 3, 14, 9, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "18:00", 4, null, null, 1, 3, 10, 15, null);
+VALUES ("2025-03-08", "18:00", 4, 1, 1, 1, 3, 10, 15, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "20:30", 4, null, null, 1, 3, 5, 12, null);
+VALUES ("2025-03-08", "20:30", 4, 1, 3, 1, 3, 5, 12, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "20:30", 4, null, null, 1, 3, 1, 7, null);
+VALUES ("2025-03-08", "20:30", 4, 3, 1, 1, 3, 1, 7, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "12:00", 4, null, null, 1, 3, 4, 2, null);
+VALUES ("2025-03-09", "12:00", 4, 1, 1, 1, 3, 4, 2, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "18:00", 4, null, null, 1, 3, 11, 6, null);
+VALUES ("2025-03-09", "18:00", 4, 1, 1, 1, 3, 11, 6, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "20:30", 4, null, null, 1, 3, 3, 8, null);
+VALUES ("2025-03-09", "20:30", 4, 2, 0, 1, 3, 3, 8, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
 VALUES ("2025-03-10", "20:00", 4, null, null, 1, 3, 13, 16, null);
 
 -- Fecha 5
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-14", "18:00", 5, null, null, 1, 3, 1, 16, null);
+VALUES ("2025-03-14", "18:00", 5, 1, 1, 1, 3, 1, 16, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-14", "20:30", 5, null, null, 1, 3, 7, 9, null);
+VALUES ("2025-03-14", "20:30", 5, 2, 3, 1, 3, 7, 9, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-15", "18:00", 5, null, null, 1, 3, 15, 3, null);
+VALUES ("2025-03-15", "18:00", 5, null, null, 3, 3, 15, 3, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-15", "20:30", 5, null, null, 1, 3, 8, 4, null);
+VALUES ("2025-03-15", "20:30", 5, 0, 0, 1, 3, 8, 4, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-16", "12:00", 5, null, null, 1, 3, 6, 14, null);
+VALUES ("2025-03-16", "12:00", 5, 1, 0, 1, 3, 6, 14, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-16", "18:00", 5, null, null, 1, 3, 2, 11, null);
+VALUES ("2025-03-16", "18:00", 5, 0, 0, 1, 3, 2, 11, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-16", "18:00", 5, null, null, 1, 3, 12, 10, null);
+VALUES ("2025-03-16", "18:00", 5, 2, 0, 1, 3, 12, 10, null);
 INSERT INTO t_match (match_date, match_kick_off,matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-16", "20:30", 5, null, null, 1, 3, 5, 13, null);
+VALUES ("2025-03-16", "20:30", 5, null, null, 3, 3, 5, 13, null);
 
 -- Fecha 6
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
@@ -112,6 +112,7 @@ INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away
 VALUES ("2025-03-30", "18:00", 6, null, null, 1, 3, 4, 1, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
 VALUES ("2025-03-30", "20:30", 6, null, null, 1, 3, 14, 2, null);
+
 
 -- Matches Primera B 2025
 -- Fecha 1
@@ -152,22 +153,57 @@ VALUES ("2025-03-02", "20:30", 2, 1, 1, 2, 4, 18, 21, null);
 
 -- Fecha 3
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-07", "18:00", 3, null, null, 1, 4, 25, 28, null);
+VALUES ("2025-03-07", "18:00", 3, 0, 1, 2, 4, 25, 28, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-07", "20:30", 3, null, null, 1, 4, 17, 32, null);
+VALUES ("2025-03-07", "20:30", 3, 0, 2, 2, 4, 17, 32, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "18:00", 3, null, null, 1, 4, 24, 31, null);
+VALUES ("2025-03-08", "18:00", 3, 2, 1, 2, 4, 24, 31, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "18:00", 3, null, null, 1, 4, 21, 26, null);
+VALUES ("2025-03-08", "18:00", 3, 3, 0, 2, 4, 21, 26, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "12:00", 3, null, null, 1, 4, 20, 22, null);
+VALUES ("2025-03-09", "12:00", 3, 0, 1, 2, 4, 20, 22, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "18:00", 3, null, null, 1, 4, 29, 18, null);
+VALUES ("2025-03-09", "18:00", 3, 0, 0, 2, 4, 29, 18, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "20:30", 3, null, null, 1, 4, 27, 23, null);
+VALUES ("2025-03-09", "20:30", 3, 1, 1, 2, 4, 27, 23, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-10", "20:00", 3, null, null, 1, 4, 30, 19, null);
+VALUES ("2025-03-10", "20:00", 3, 4, 0, 2, 4, 30, 19, null);
 
+-- Fecha 4
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-14", "18:00", 4, 0, 1, 2, 4, 31, 21, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-14", "20:15", 4, 1, 2, 2, 4, 32, 27, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-15", "18:00", 4, 1, 1, 2, 4, 26, 17, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-16", "12:00", 4, 1, 2, 2, 4, 28, 30, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-16", "20:30", 4, 2, 0, 2, 4, 23, 20, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-17", "18:00", 4, null, null, 1, 4, 22, 29, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-17", "18:00", 4, null, null, 1, 4, 19, 24, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-17", "20:30", 4, null, null, 1, 4, 18, 25, null);
+
+-- Fecha 5
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-28", "18:15", 5, null, null, 2, 4, 17, 19, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-28", "20:30", 5, null, null, 2, 4, 32, 23, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-29", "18:00", 5, null, null, 2, 4, 31, 22, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-30", "18:00", 5, null, null, 2, 4, 25, 27, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-30", "20:30", 5, null, null, 2, 4, 18, 20, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-31", "18:00", 5, null, null, 1, 4, 29, 26, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-31", "20:30", 5, null, null, 1, 4, 24, 28, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-31", "20:30", 5, null, null, 1, 4, 21, 30, null);
 
 -- Matches Segunda 2025
 --Fecha 1
@@ -184,20 +220,52 @@ VALUES ("2025-03-02", "20:30", 1, 2, 1, 2, 5, 43, 35, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
 VALUES ("2025-03-03", "20:30", 1, 1, 1, 2, 5, 38, 41, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES (null, null, null, null, null, 3, 5, 42, 46, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
+VALUES (null, null, 1, null, null, 3, 5, 42, 46, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
 
 -- Fecha 2
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "18:00", 2, null, null, 1, 5, 36, 34, null);
+VALUES ("2025-03-08", "18:00", 2, 3, 2, 2, 5, 36, 34, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "18:00", 2, null, null, 1, 5, 37, 43, null);
+VALUES ("2025-03-08", "18:00", 2, 2, 2, 2, 5, 37, 43, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-08", "20:30", 2, null, null, 1, 5, 35, 42, null);
+VALUES ("2025-03-08", "20:30", 2, 1, 1, 2, 5, 35, 42, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "18:00", 2, null, null, 1, 5, 39, 40, null);
+VALUES ("2025-03-09", "18:00", 2, 3, 3, 2, 5, 39, 40, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-09", "20:30", 2, null, null, 1, 5, 45, 33, null);
+VALUES ("2025-03-09", "20:30", 2, 3, 1, 2, 5, 45, 33, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES ("2025-03-10", "20:30", 2, null, null, 1, 5, 41, 44, null);
+VALUES ("2025-03-10", "20:30", 2, 0, 0, 2, 5, 41, 44, null);
 INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
-VALUES (null, null, null, null, null, 3, 5, 46, 38, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
+VALUES (null, null, 2, null, null, 3, 5, 46, 38, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
+
+-- Fecha 3
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-15", "18:00", 3, 3, 1, 2, 5, 40, 45, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-15", "18:00", 3, 1, 3, 2, 5, 42, 37, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-15", "20:30", 3, 0, 2, 2, 5, 43, 41, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-16", "18:00", 3, 3, 1, 2, 5, 38, 36, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-16", "18:00", 3, 0, 1, 2, 5, 34, 35, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-16", "18:00", 3, 1, 1, 2, 5, 44, 39, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES (null, null, 3, null, null, 3, 5, 46, 33, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
+
+-- Fecha 4
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-28", "18:00", 4, null, null, 1, 5, 43, 42, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-29", "18:00", 4, null, null, 1, 5, 35, 33, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-29", "18:00", 4, null, null, 1, 5, 41, 40, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-30", "18:00", 4, null, null, 1, 5, 39, 36, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-30", "18:00", 4, null, null, 1, 5, 37, 38, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES ("2025-03-30", "20:30", 4, null, null, 1, 5, 45, 34, null);
+INSERT INTO t_match (match_date, match_kick_off, matchday, home_team_goals, away_team_goals, match_status_id, tournament_id, home_team_id, away_team_id, notes)
+VALUES (null, null, 4, null, null, 3, 5, 46, 44, "AC Barnechea declinó participar del campeonato Segunda La Liga 2D 2025.");
