eac091dba9e97467aa94d156f9dde544c50694c2
criszav <czavala.dev@gmail.com>
2025-04-30 16:10:26 -0400

    refactor(data): modificar sentencias insert cards, goals, player, user para testing

--- src/main/resources/db/player.sql
+++ src/main/resources/db/player.sql
@@ -1,41 +1,12 @@
--- Concon National
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Esteban", "Kirkman", null, null, 1, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Brian", "Torres", null, null, 12, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Cristian", "Vega", null, null, 23, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Javier", "Retamales", null, null, 7, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Ricardo", "Fuenzalida", null, null, 17, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Matias", "Leiva", null, null, 18, true, true, 1)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Christian", "Alba", null, null, 24, true, true, 1)
-
--- Concepcion
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Nicolas", "Araya", null, null, 25, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Lautaro", "Rigazzi", null, null, 2, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Sebastian", "Silva", null, null, 5, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Brayan", "Valdivia", null, null, 11, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Sebastian", "Martinez", null, null, 14, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Ignacio", "Mesias", null, null, 27, true, true, 2)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Carlos", "Escobar", null, null, 9, true, true, 2)
-
--- Linares
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Gustavo", "Fuenzalida", null, null, 1, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Franco", "Flores", null, null, 2, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Flavio", "Rojas", null, null, 3, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Franz", "Schultz", null, null, 14, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("José", "Molina", null, null, 21, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Nicolás", "Forttes", null, null, 13, true, true, 3)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Maximiliano", "Cerato", null, null, 17, true, true, 3)
-
 -- Melipilla
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Darío", "Melo", "Jocker", null, 22, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Cristóbal", "Vergara", "El Patrón", null, 19, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Erick", "Millalén", "Milla", null, 5, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Matías", "Rodríguez", "Mati", null, 6, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Critian", "Magaña", null, null, 17, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Albano", "Becica", "El Abuelo", null, 29, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Valentin", "Demateis", null, null, 30, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Josue", "Ovalle", "Cotto", null, 10, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Bryan", "Taiva", null, null, 14, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Franco", "Ortega", null, null, 11, true, true, 4)
-INSERT INTO t_player(firstname, lastname, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Luis", "Vargas", "Tanque", null, 9, true, true, 4)
-
--- Puerto Montt
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Darío", "Melo", "123456780", "1992-03-15", "Jocker", null, 22, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Cristóbal", "Vergara", "234567890", "1995-07-22", "El Patrón", null, 19, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Erick", "Millalén", "345678901", "1998-12-05", "Milla", null, 5, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Matías", "Rodríguez", "456789012", "1991-04-19", "Mati", null, 6, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Cristian", "Magaña", "567890123", "1996-09-11", "Maga", null, 17, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Albano", "Becica", "678901234", "1999-02-28", "El Abuelo", null, 29, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Valentin", "Demateis", "789012345", "1987-08-03", null, null, 30, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Josue", "Ovalle", "890123456", "1997-11-27", "Cotto", null, 10, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Bryan", "Taiva", "901234567", "1994-06-16", null, null, 14, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Franco", "Ortega", "012345678", "1990-01-09", null, null, 11, true, true, 4)
+INSERT INTO t_player(firstname, lastname, run, birth_date, nickname, image_url, squad_number, is_active, is_enabled, team_id) VALUES ("Luis", "Vargas", "135792468", "2000-05-20",  "Tanque", null, 9, true, true, 4)
