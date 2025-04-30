ba7829e0f6aab66713fe766ed8fa662b8e67a032
criszav <czavala.dev@gmail.com>
2025-04-30 16:09:15 -0400

    feat(player): capturar y manejar excepción al crear player con numero de camiseta duplicada
    PlayerServiceImpl:
    - Manejo de error a violación de unique constraint 'UK_PLAYER_SQUAD_TEAM' al actualizar player con numero de camiseta que esta en uso.

--- src/main/java/com/czavala/football_tournament_manager/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/czavala/football_tournament_manager/service/impl/PlayerServiceImpl.java
@@ -10,11 +10,13 @@ import com.czavala.football_tournament_manager.persistance.entity.Player;
 import com.czavala.football_tournament_manager.persistance.repository.PlayerRepository;
 import com.czavala.football_tournament_manager.service.PlayerService;
 import com.czavala.football_tournament_manager.service.cloudinary.CloudinaryService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -52,6 +54,8 @@ public class PlayerServiceImpl implements PlayerService {
         Player newPlayer = new Player();
         newPlayer.setFirstname(playerDto.getFirstname());
         newPlayer.setLastname(playerDto.getLastname());
+        newPlayer.setRun(playerDto.getRunPlayer());
+        newPlayer.setBirthDate(playerDto.getBirthDate());
         newPlayer.setNickname(playerDto.getNickname());
         newPlayer.setSquadNumber(playerDto.getSquadNumber());
         newPlayer.setActive(playerDto.getIsActive());
@@ -76,38 +80,67 @@ public class PlayerServiceImpl implements PlayerService {
     @Override
     public PlayerResponseDto updateById(Long playerId, SavePlayerDto playerDto) {
 
-        // Valida si el número de camiseta del nuevo jugador ya existe
-        validateSquadNumberExists(playerDto);
+        try {
 
-        // Obtiene desde db al jugador que se quiere actualizar
-        Player playerFromDB = this.findPlayerEntityById(playerId);
-        playerFromDB.setFirstname(playerDto.getFirstname());
-        playerFromDB.setLastname(playerDto.getLastname());
-        playerFromDB.setNickname(playerDto.getNickname());
-        playerFromDB.setSquadNumber(playerDto.getSquadNumber());
-        playerFromDB.setTeamId(playerDto.getTeamId());
-        playerFromDB.setActive(playerDto.getIsActive());
-        playerFromDB.setEnabled(playerDto.getIsEnabled());
+            // Obtiene desde db al jugador que se quiere actualizar
+            Player playerFromDB = this.findPlayerEntityById(playerId);
+            playerFromDB.setFirstname(playerDto.getFirstname());
+            playerFromDB.setLastname(playerDto.getLastname());
+            playerFromDB.setRun(playerDto.getRunPlayer());
+            playerFromDB.setBirthDate(playerDto.getBirthDate());
+            playerFromDB.setNickname(playerDto.getNickname());
+            playerFromDB.setSquadNumber(playerDto.getSquadNumber());
+            playerFromDB.setTeamId(playerDto.getTeamId());
+            playerFromDB.setActive(playerDto.getIsActive());
+            playerFromDB.setEnabled(playerDto.getIsEnabled());
 
-        // Verifica si la petición contiene una nueva imagen
-        // Si petición NO contiene una nueva imagen entonces se mantiene url de imagen existente
-        if (playerDto.getImageFile() != null && !playerDto.getImageFile().isEmpty()) {
+            // Verifica si la petición contiene una nueva imagen
+            // Si petición NO contiene una nueva imagen entonces se mantiene url de imagen existente
+            if (playerDto.getImageFile() != null && !playerDto.getImageFile().isEmpty()) {
 
-            // Sube nueva imagen a Cloudinary y obtiene nueva url
-            String imageUrl = cloudinaryService.uploadImageToCloudinary(
-                    playerDto.getImageFile(),
-                    "teams", // Indica que se guardará en carpeta 'players' en Cloudinary
-                    playerDto.getFirstname().toLowerCase() + "-" + playerDto.getLastname().toLowerCase() // Nombre que se asignará a imagen (nombre del jugador)
-            );
+                // Sube nueva imagen a Cloudinary y obtiene nueva url
+                String imageUrl = cloudinaryService.uploadImageToCloudinary(
+                        playerDto.getImageFile(),
+                        "players", // Indica que se guardará en carpeta 'players' en Cloudinary
+                        playerDto.getFirstname().toLowerCase() + "-" + playerDto.getLastname().toLowerCase() // Nombre que se asignará a imagen (nombre del jugador)
+                );
 
-            // Actualiza nueva url de la imagen del team en db
-            playerFromDB.setImageUrl(imageUrl);
+                // Actualiza nueva url de la imagen del team en db
+                playerFromDB.setImageUrl(imageUrl);
+            }
+            playerFromDB.setLastModified(LocalDateTime.now(ZoneId.systemDefault()));
+
+            // Guarda en db player actualizado
+            playerRepository.save(playerFromDB);
+
+            return PlayerMapper.mapToPlayerResponseDto(playerFromDB);
+
+        } catch (DataIntegrityViolationException exception) {
+
+            // Obtiene la causa raíz de la excepción
+            Throwable rootCause = exception.getRootCause();
+
+            // Verifica que la causa raíz de la excepción sea una violación de constraint
+            if (rootCause instanceof SQLIntegrityConstraintViolationException sqlException) {
+
+                // Obtiene mensaje de error desde db
+                String sqlErrorMsg = sqlException.getMessage();
+
+                // Verifica que el error sea una violación a unique constraint 'UK_PLAYER_SQUAD_TEAM'
+                if (sqlErrorMsg != null && sqlErrorMsg.contains("UK_PLAYER_SQUAD_TEAM")) {
+
+                    // Mensaje de error para el usuario
+                    String errorMsg = "El número de camiseta " + playerDto.getSquadNumber() +
+                            " ya está en uso. Por favor ingrese un número de camiseta válido.";
+
+                    // Lanza excepción indicando que número de camiseta del jugador ya está en uso
+                    throw new DuplicateSquadNumberException(errorMsg, sqlErrorMsg);
+                }
+            }
+
+            // Lanza excepción genérica si causa raíz del error NO es una violación a una constraint
+            throw exception;
         }
-        playerFromDB.setLastModified(LocalDateTime.now(ZoneId.systemDefault()));
-
-        playerRepository.save(playerFromDB);
-
-        return PlayerMapper.mapToPlayerResponseDto(playerFromDB);
     }
 
     @Override
